5b94a61e43541c0f54472a7fb335283401a0bc08
dinolirios <thedlirios@gmail.com>
2025-02-03 17:29:51 +0800

    Drawing in our ship

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)  # Draw the player
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__ == "__main__":
     main()
